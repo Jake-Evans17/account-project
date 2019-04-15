96f74b206a15562a34922c653c7859ea0653bef7
Jake-Evans17 <Jake.Evans@academytrainee.com>
2019-04-15 14:26:45 +0100

    first commit

---
+++ src/main/java/com/qa/account_application/AccountApplication/App.java
@@ -0,0 +1,9 @@
+package com.qa.account_application.AccountApplication;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello world to the standard out" );
+    }
+}
